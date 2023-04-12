e7ec92ebc4227f427e2ebbbc31ed18e18544d021
Bilal-BM <bilal.bm9665@gmail.com>
2023-04-12 15:07:21 +0500

    update homepage

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -28,7 +28,7 @@ const Home = () => {
     <Navbar />
     <div className="container mx-auto py-4">
       <h1 className="text-3xl font-bold mb-4">Latest News</h1>
-      <div className="grid  grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
         {news.map((article) => (
           <Article key={article.url} article={article} />
         ))}
